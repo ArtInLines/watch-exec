b2864d187cc8de4352344b49d00dba7360d511e2
ArtInLines <64645307+ArtInLines@users.noreply.github.com>
2024-10-06 00:45:10 +0200

    remove unused dependency

--- build-linux.sh
+++ build-linux.sh
@@ -1,3 +1,3 @@
 set -xe
 
-clang -o watch-exec src/main.c -lpthread -Wall -Wextra -Wpedantic -Wno-unused-function -Werror -g
+clang -o watch-exec src/main.c -Wall -Wextra -Wpedantic -Wno-unused-function -Werror -g
--- build-mac.sh
+++ build-mac.sh
@@ -1,3 +1,3 @@
 set -xe
 
-clang -o watch-exec src/main.c -framework CoreFoundation -framework CoreServices -lpthread -Wall -Wextra -Wpedantic -Wno-unused-function -Werror -g
+clang -o watch-exec src/main.c -framework CoreFoundation -framework CoreServices -Wall -Wextra -Wpedantic -Wno-unused-function -Werror -g
